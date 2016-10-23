9e1acad8cd3fa17b9e23b0e0941fc97e6e9d2e35
David Law <davidsamuellaw@gmail.com>
2016-10-22 19:46:59 -0700

    Add parsing and tests

---
+++ R/extract.R
@@ -0,0 +1,171 @@
+# https://github.com/krisajenkins/yesql/blob/master/src/yesql/queryfile.bnf
+
+
+comment_ <- stringr::regex("^\\s*--")
+name_tag <- stringr::fixed("name:")
+delimiter <- ";"
+placeholder <- stringr::regex("\\?")
+parameter <- stringr::regex(":\\w+")
+
+
+syntax_error <- function(msg = "Invalid syntax") {
+    stop(msg, call. = FALSE)
+}
+
+rm_pattern <- function(string, pattern) {
+    stringr::str_replace(string, pattern, "")
+}
+
+blank_line <- function(string) {
+    stringr::str_detect(string, stringr::boundary("word"))
+}
+
+#' Split slurped SQL into named-queries.
+#'
+#' @param lines Character vector of SQL statements to split.
+#' @return queries List of queries.
+split_queries <- function(lines) {
+    trimmed <- Filter(blank_line, lines)
+    line_comment <- stringr::str_detect(trimmed, comment_)
+    name <- stringr::str_detect(trimmed, name_tag)
+    delim <- stringr::str_detect(trimmed, delimiter)
+
+    begin_indexes <- which(line_comment & name)
+    end_indexes <- which(!line_comment & delim)
+
+    # Check every name has a query.
+    if (length(begin_indexes) != length(end_indexes) ||
+        any((end_indexes - begin_indexes) < 1L)) {
+        syntax_error()
+    }
+
+    mapply(function(i, j) trimmed[i:j],
+           begin_indexes,
+           end_indexes,
+           SIMPLIFY = FALSE)
+}
+
+#' Check if the line contains a query name.
+is_name <- function(string) {
+    name_pattern <- stringr::str_c(comment_, "\\s*", name_tag, ".+?")
+    stringr::str_detect(string, stringr::regex(name_pattern))
+}
+
+#' Get query name.
+#'
+#' Extract the name of the query from the docstring.
+#' @param string Character vector giving the query.
+#' @return query_name Character string.
+extract_name <- function(string) {
+    # Input should be validated as a name line prior to passing in.
+    pattern <- stringr::str_c("^.*", name_tag)
+    query_name <- rm_pattern(string, pattern)
+
+    # Validate remaining text.
+    # Replace dashes with underscores so it counts as a single word.
+    snake_case <- stringr::str_replace_all(query_name, "-", "_")
+    n_words <- stringr::str_count(snake_case, stringr::boundary("word"))
+
+    if (n_words != 1L) {
+        msg <- paste0("'", string, "'", " does not contain a valid name")
+        syntax_error(msg)
+    }
+
+    stringr::str_trim(query_name)
+}
+
+#' Get query description.
+#'
+#' Extract the query description from the docstring.
+#' @param lines Character vector giving the query docstring.
+#' @return description Character vector.
+extract_description <- function(lines) {
+    comment_pattern <- stringr::str_c(comment_, "[ ]")
+
+    docstring <- stringr::str_subset(lines, comment_)
+    description <- rm_pattern(docstring, stringr::regex(comment_pattern))
+    if (length(description)) description else NA
+}
+
+#' Get SQL query.
+#'
+#' Extract the SQL query.
+#' @param lines Character vector giving the full query.
+#' @return sql Character string.
+extract_sql <- function(lines) {
+    is_query <- !stringr::str_detect(lines, comment_)
+    sql <- lines[is_query]
+    # Queries using DBI must be a length one char vector.
+    stringr::str_c(sql, collapse = "\n")
+}
+
+#' Determine the type of query.
+#'
+#' Check the query name for identifying information.
+#' @param query_name Character string. The parsed query name.
+#' @return type Character string.
+query_type <- function(query_name) {
+    # 'reads' will always match (default case).
+    types <- c(returns = "<!", modifies = "!", reads = ".*")
+    matches <- stringr::str_detect(query_name, types)
+    matched_types <- names(types)[matches]
+    matched_types[1L]
+}
+
+#' Check if the query contains parameters.
+is_prepared <- function(sql) {
+    # Remove comments to avoid false positives.
+    line_comment <- stringr::regex("--.*$")
+    block_comment <- stringr::regex("/\\*.*\\*/", multiline = TRUE)
+    # Vectorize over `sql`.
+    stripped <- rm_pattern(rm_pattern(sql, block_comment), line_comment)
+
+    detected_params <- stringr::str_detect(stripped, parameter)
+    detected_placeholders <- stringr::str_detect(stripped, placeholder)
+    any(c(detected_params, detected_placeholders))
+}
+
+#' Parse a docstring with name and sql block.
+parse_named_query <- function(block) {
+    first <- head(block, n = 1L)
+    rest <- tail(block, n = -1L)
+
+    if (!is_name(first)) {
+        msg <- paste0("Query must have a ", name_tag, " tag in the first line")
+        syntax_error(msg)
+    }
+
+    # Perform cursory validation on SQL.
+    sql <- extract_sql(rest)
+    if (stringr::str_count(sql, delimiter) > 1L) {
+        syntax_error("Cannot process multiple statements in a single query")
+    }
+
+    query <- list()
+    class(query) <- c("query", "named", class(query))
+
+    query$name <- extract_name(first)
+    query$type <- query_type(query$name)
+    query$description <- extract_description(rest)
+    query$sql <- rm_pattern(sql, delimiter)
+    query$prepared <- is_prepared(sql)
+    query
+}
+
+#' Parse a docstring and sql block.
+parse_anon_query <- function(block) {
+    # Perform cursory validation on SQL.
+    sql <- extract_sql(block)
+    if (stringr::str_count(sql, delimiter) > 1L) {
+        syntax_error("Multiple statements detected")
+    }
+
+    query <- list()
+    class(query) <- c("query", class(query))
+
+    query$description <- extract_description(block)
+    query$sql <- rm_pattern(sql, delimiter)
+    query$prepared <- is_prepared(sql)
+    query
+}
+
---
+++ tests/testthat/test-extract.R
@@ -0,0 +1,215 @@
+library(ryesql)
+context("Parse SQL files")
+
+
+test_that("split_queries splits named-queries", {
+    txt <- c(
+        "-- File description. ",
+        "",
+        "-- name: my-query",
+        "-- Queries for foo.",
+        "SELECT foo",
+        "FROM tbl",
+        "GROUP BY bar;",
+        "",
+        "-- name: update!",
+        "-- Updates foo.",
+        "UPDATE tbl",
+        "SET foo=1",
+        "WHERE bar < 10;",
+        "")
+    expected <- list(
+        c("-- name: my-query",
+          "-- Queries for foo.",
+          "SELECT foo",
+          "FROM tbl",
+          "GROUP BY bar;"),
+        c("-- name: update!",
+          "-- Updates foo.",
+          "UPDATE tbl",
+          "SET foo=1",
+          "WHERE bar < 10;"))
+
+    queries <- split_queries(txt)
+    expect_equal(queries, expected)
+})
+
+test_that("split_queries throws for invalid queries", {
+    txt <- c("-- name: get-foos",
+             "-- Gets the foos.",
+             "",
+             "-- name: get-ids",
+             "-- Gets ids.",
+             "SELECT id",
+             "FROM tbl;")
+    expect_error(split_queries(txt), "Invalid syntax")
+})
+
+test_that("is_name identifies valid names", {
+    expect_true( is_name("-- name: foo-bar") )
+
+    # Modifying query.
+    expect_true( is_name("-- name: foo-bar!") )
+
+    # Returning query.
+    expect_true( is_name("-- name: foo-bar<!") )
+
+    # Leading/trailing whitespace.
+    expect_true( is_name(" -- name: foo-bar ") )
+
+    # Only check for name identifier - don't validate names.
+    expect_true( is_name("-- name: foo bar") )
+})
+
+test_that("is_name identifies non-names", {
+    expect_false( is_name("-- foo-bar") )
+    expect_false( is_name("/* name: foo-bar */") )
+})
+
+test_that("extract_name gets name", {
+    expected <- "foo-bar"
+
+    out <- extract_name("-- name: foo-bar")
+    expect_equal(out, expected)
+
+    # Modifying query.
+    out <- extract_name("-- name: foo-bar!")
+    expect_equal(out, "foo-bar!")
+
+    # Returning query.
+    out <- extract_name("-- name: foo-bar<!")
+    expect_equal(out, "foo-bar<!")
+
+    # Leading/trailing whitespace.
+    out <- extract_name(" --name: foo-bar ")
+    expect_equal(out, expected)
+
+    # No space between name and name-identifier.
+    out <- extract_name("--name:foo-bar")
+    expect_equal(out, expected)
+})
+
+test_that("extract_name throws for invalid name", {
+    expected_msg_partial <- "does not contain a valid name"
+
+    expect_error(extract_name("-- name: foo bar"), expected_msg_partial)
+    expect_error(extract_name("-- name: foo, bar"), expected_msg_partial)
+    expect_error(extract_name("-- name: foo (does bar)"), expected_msg_partial)
+    expect_error(extract_name("-- name: foo!!bar"), expected_msg_partial)
+})
+
+test_that("extract_description gets description", {
+    txt <- c("-- Gets the foos.",
+             "--   bar : filter by",
+             "SELECT foo",
+             "FROM tbl",
+             "WHERE id = :bar;")
+    expected <- c("Gets the foos.",
+                  "  bar : filter by")
+    expect_equal(extract_description(txt), expected)
+
+    # No description.
+    txt <- c("SELECT foo",
+             "FROM tbl",
+             "WHERE id = :bar;")
+    expect_equal(extract_description(txt), NA)
+})
+
+test_that("extract_sql gets sql", {
+    txt <- c("-- Gets the foos.",
+             "--   bar : filter by",
+             "SELECT foo",
+             "FROM tbl",
+             "WHERE id = :bar;")
+    expected <- "SELECT foo\nFROM tbl\nWHERE id = :bar;"
+    expect_equal(extract_sql(txt), expected)
+})
+
+test_that("query_type identifies query from name", {
+    expect_equal(query_type("modify!"), "modifies")
+    expect_equal(query_type("get-results<!"), "returns")
+    expect_equal(query_type("read-tbl"), "reads")
+})
+
+test_that("is_prepared identifies parameterized queries", {
+    # Named parameter.
+    sql <- c("SELECT *",
+             "FROM tbl",
+             "WHERE id > :i;")
+    expect_true(is_prepared(sql))
+
+    # Snake case.
+    sql <- c("SELECT *",
+             "FROM tbl",
+             "WHERE id > :foo_bar;")
+    expect_true(is_prepared(sql))
+
+    # Anonymous parameter/placeholder.
+    sql <- c("SELECT *",
+             "FROM tbl",
+             "WHERE id > ?;")
+    expect_true(is_prepared(sql))
+
+    # No parameter.
+    sql <- c("SELECT *",
+             "FROM tbl",
+             "WHERE id > 4;")
+    expect_false(is_prepared(sql))
+
+    # Parameter in comment.
+    sql <- c("SELECT *",
+             "FROM tbl",
+             "-- should use placeholder eg ?",
+             "WHERE id > 10;")
+    expect_false(is_prepared(sql))
+
+    sql <- c("SELECT *",
+             "FROM tbl",
+             "WHERE id > 10; /* ignore :i */")
+    expect_false(is_prepared(sql))
+})
+
+test_that("parse_named_query parses named-query", {
+    txt <- c("-- name: add-event!",
+             "-- Add an event.",
+             "INSERT INTO events",
+             "VALUES (?, ?, ?);")
+    out <- parse_named_query(txt)
+
+    expect_equal(class(out), c("query", "named", "list"))
+    expect_equal(out$name, "add-event!")
+    expect_equal(out$type, "modifies")
+    expect_equal(out$description, "Add an event.")
+    expect_equal(out$sql, "INSERT INTO events\nVALUES (?, ?, ?)") # no semicolon
+    expect_true(out$prepared)
+})
+
+test_that("parse_named_query throws for multiple statements", {
+    invalid <- c("-- name: add-event!",
+                 "INSERT INTO events",
+                 "VALUES (?, ?, ?);",
+                 "",
+                 "-- name: get-event",
+                 "SELECT * FROM events;")
+    expect_error(parse_named_query(invalid), "multiple statements")
+})
+
+test_that("parse_anon_query parses query", {
+    txt <- c("-- Add an event.",
+             "INSERT INTO events",
+             "VALUES (?, ?, ?);")
+    out <- parse_anon_query(txt)
+
+    expect_equal(class(out), c("query", "list"))
+    expect_equal(out$description, "Add an event.")
+    expect_equal(out$sql, "INSERT INTO events\nVALUES (?, ?, ?)") # no semicolon
+    expect_true(out$prepared)
+})
+
+test_that("parse_anon_query throws for multiple statements", {
+    invalid <- c("SELECT * FROM events;",
+                 "",
+                 "SELECT * FROM events WHERE id > 10;")
+    expect_error(parse_anon_query(invalid), "Multiple statements")
+})
+
